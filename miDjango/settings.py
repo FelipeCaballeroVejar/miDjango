3b33ab520e9d9e07768fa2e8fa5282d6a4b1c51f
GonzaloEspinoza <128237859+KingDmnT@users.noreply.github.com>
2023-07-11 15:34:11 -0400

    se ve la conexion a base de datos

--- miDjango/settings.py
+++ miDjango/settings.py
@@ -45,12 +45,12 @@ INSTALLED_APPS = [
     'core',
     'django.contrib.humanize',
     "crispy_forms",
-    "crispy_bootstrap5",
+    # "crispy_bootstrap5",
 ]
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+# # CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
-CRISPY_TEMPLATE_PACK = "bootstrap5"
+# # CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -90,7 +90,7 @@ DATABASES = {
     'default': {
        'ENGINE': 'django.db.backends.sqlite3',
       'NAME': BASE_DIR / 'db.sqlite3',
-    },
+    }, 
 }
 
 
